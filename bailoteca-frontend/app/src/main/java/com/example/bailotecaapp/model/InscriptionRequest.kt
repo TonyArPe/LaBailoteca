b398cef647141ccad9d8bda2170ae89ab4077827
TonyArPe <tonyaragonp@gmail.com>
2025-05-27 13:23:10 +0200

    Borrado de duplicidades

--- bailoteca-frontend/app/src/main/java/com/example/bailotecaapp/model/InscriptionRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bailotecaapp.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class InscripcionRequest(
-    val usuarioId: Long,
-    val claseId: Long
-)
