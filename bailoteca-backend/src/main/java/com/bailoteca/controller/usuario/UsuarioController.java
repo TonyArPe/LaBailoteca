54abbadd1148cead627168988bb189726148b90f
TonyArPe <tonyaragonp@gmail.com>
2025-05-29 12:19:16 +0200

    Implentado borrado en repo HorarioClase

--- bailoteca-backend/src/main/java/com/bailoteca/controller/usuario/UsuarioController.java
+++ bailoteca-backend/src/main/java/com/bailoteca/controller/usuario/UsuarioController.java
@@ -16,7 +16,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
 /**
--- bailoteca-backend/src/main/java/com/bailoteca/repository/clase/HorarioClaseRepo.java
+++ bailoteca-backend/src/main/java/com/bailoteca/repository/clase/HorarioClaseRepo.java
@@ -1,13 +1,19 @@
 package com.bailoteca.repository.clase;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.bailoteca.models.clase.HorarioClase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface HorarioClaseRepo extends JpaRepository<HorarioClase, Long> {
+
     List<HorarioClase> findByClaseId(Long claseId);
+
+    @Modifying
+    @Transactional
+    void deleteByClaseId(Long claseId);
 }
