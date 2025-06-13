967d6f8e0a8d0c795435c017191f65b2a9f18f80
TonyArPe <tonyaragonp@gmail.com>
2025-06-13 09:46:14 +0200

    modo emulador

--- bailoteca-frontend/app/src/main/java/com/example/bailotecaapp/di/NetworkModule.kt
+++ bailoteca-frontend/app/src/main/java/com/example/bailotecaapp/di/NetworkModule.kt
@@ -22,7 +22,7 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "https://fe65-84-122-0-141.ngrok-free.app/"
+    private const val BASE_URL = "http://10.0.2.2:8080/"
 
     /**
      * Proporciona el interceptor que añade y renueva el token JWT de Firebase.
